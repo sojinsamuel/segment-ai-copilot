84a4834692922d97c408775fa7582601a9418c3b
Vaith Schmitz <vschmitz@jm91w7wnmq.cust.communityfibre.co.uk>
2024-03-20 13:11:21 +0000

    removed example messages

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -22,61 +22,14 @@ export interface ChatPanelProps {
 export function ChatPanel({ id, title, input, setInput }: ChatPanelProps) {
   const [aiState] = useAIState()
   const [messages, setMessages] = useUIState<typeof AI>()
-  const { submitUserMessage } = useActions()
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
 
-  const exampleMessages = [
-    // {
-    //   heading: 'Explain the concept',
-    //   subheading: 'of portfolio diversification',
-    //   message: `Explain the concept of a portfolio diversification`
-    // },
-    // {
-    //   heading: 'What are the benefits',
-    //   subheading: 'of buying bonds and stocks',
-    //   message: 'What are the benefits of buying bonds and stocks?'
-    // },
-  ]
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
       <ButtonScrollToBottom />
 
       <div className="mx-auto sm:max-w-2xl sm:px-4">
-        <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
-          {messages.length === 0 &&
-            exampleMessages.map((example, index) => (
-              <div
-                key={example.heading}
-                className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
-                  index > 1 && 'hidden md:block'
-                }`}
-                onClick={async () => {
-                  setMessages(currentMessages => [
-                    ...currentMessages,
-                    {
-                      id: nanoid(),
-                      display: <UserMessage>{example.message}</UserMessage>
-                    }
-                  ])
-
-                  const responseMessage = await submitUserMessage(
-                    example.message
-                  )
-
-                  setMessages(currentMessages => [
-                    ...currentMessages,
-                    responseMessage
-                  ])
-                }}
-              >
-                <div className="text-sm font-semibold">{example.heading}</div>
-                <div className="text-sm text-zinc-600">
-                  {example.subheading}
-                </div>
-              </div>
-            ))}
-        </div>
 
         {messages?.length >= 2 ? (
           <div className="flex h-12 items-center justify-center">
